aa8a5e59aaa3b7c122734d76c224a55eb6cf5c8b
gustavoclay <gustavoclay@gmail.com>
2024-03-07 21:59:45 -0300

    inicializando lista de exercicios

---
+++ listaExercicios/exercicios/exercicio1.js
@@ -0,0 +1,3 @@
+// 1. Faça um Programa que receba quatro notas de um aluno, calcule e imprima a média aritmética das notas e a mensagem de aprovado para média superior ou igual a 7.0 ou a mensagem de reprovado para média inferior a 7.0.
+//     a.Bônus: nenhuma nota pode passar de 10.
+
---
+++ listaExercicios/exercicios/tutorial.js
@@ -0,0 +1,31 @@
+let prompt = require('prompt-sync')();
+
+let nome = prompt("Qual o seu nome?")
+
+console.log("Olá " + nome + ", Bem-vindo!")
+
+let idade = prompt("Qual a sua idade?")
+
+// console.log(typeof idade)
+
+if(idade >= 18){
+    console.log("Você é maior de idade!")
+} else {
+    console.log("Você é menor de idade!")
+}
+
+let notaProva1 = prompt("Nota prova A1?")
+let notaProva2 = prompt("Nota prova A2?")
+
+let nota1 = Number(notaProva1)
+let nota2 = Number(notaProva2)
+
+let media = (nota1 + nota2) / 2
+
+console.log("MEDIA: ", media)
+
+if(media >= 6){
+    console.log("APROVADO!")
+} else {
+    console.log("REPROVADO @!123......")
+}
