c7727c7a2074c66a9ac5b4e7cb9d03cbcb2b58a0
gustavoclay <gustavoclay@gmail.com>
2024-03-14 19:57:25 -0300

    Esquema de modulos padrão do  nodejs

---
+++ api-exemplo/Calculadora.js
@@ -0,0 +1,19 @@
+const somar = function (numA, numB) {
+    return numA + numB
+}
+
+const subtrair = function (numA, numB) {
+    return numA - numB
+}
+
+const multiplicar = function (numA, numB) {
+    return numA * numB
+}
+
+const dividir = function (numA, numB) {
+    return numA / numB
+}
+
+module.exports = {
+    somar, subtrair, multiplicar, dividir
+}
---
+++ api-exemplo/usaCalculadora.js
@@ -0,0 +1,6 @@
+const calculadora = require('./Calculadora')
+
+console.log(calculadora.somar(2,2))
+console.log(calculadora.subtrair(10,5))
+console.log(calculadora.multiplicar(10,10))
+console.log(calculadora.dividir(100,10))
