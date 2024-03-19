bd899f58110127e146806278ae31f922068ae1c8
gustavoclay <gustavoclay@gmail.com>
2024-03-18 21:57:44 -0300

    Fazendo exercicio 1 da lista 1 com express

--- 01-api-inicial/index.js
+++ 01-api-inicial/index.js
@@ -17,9 +17,9 @@ app.get('/nome', (req, res) => {
 })
 
 // CRIA UM ENDPOINT QUE DEVOLVA SEU NOME E MATRICULA /ALUNO
-app.get('/aluno', (req, res) => {
-    res.send("NOME: Gustavo Clay MATRICULA: 00000001")
-})
+// app.get('/aluno', (req, res) => {
+//     res.send("NOME: Gustavo Clay MATRICULA: 00000001")
+// })
 
 
 app.get('/teste', (req, res) => {
@@ -48,6 +48,22 @@ app.get('/pessoa', (req, res) => {
 
 
 // 1. Faça um Programa que receba quatro notas de um aluno, calcule e imprima a média aritmética das notas e a mensagem de aprovado para média superior ou igual a 7.0 ou a mensagem de reprovado para média inferior a 7.0.
+app.get('/aluno', (req, res) => {
+    console.log(req.query)
+    const nota1 = Number(req.query.nota1)
+    const nota2 = Number(req.query.nota2)
+    const nota3 = Number(req.query.nota3)
+    const nota4 = Number(req.query.nota4)
+
+    const media = (nota1 + nota2 + nota3 + nota4) / 4
+
+    const resultado = media >= 7 ? "Aprovado" : "Reprovado"
+
+    res.send(`
+        Media: ${media.toFixed(1)}
+        Resultado: ${resultado}
+    `)
+})
 
 
 
